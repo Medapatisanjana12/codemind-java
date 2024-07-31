217ce74bb6da9ad0fd40a1b6fe1e29675ee71955
Sanjana Medapati <147472809+Medapatisanjana12@users.noreply.github.com>
2024-07-31 15:23:56 +0530

    import java.util.Scanner; public class Second{     public static void main(String[] args){         int a,x,y;         Scanner read=new Scanner(System.in);         x=read.nextInt();         y=read.nextInt();         a=2*x-y;         System.out.println(a);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Second{
+    public static void main(String[] args){
+        int a,x,y;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        y=read.nextInt();
+        a=2*x-y;
+        System.out.println(a);
+    }
+}
