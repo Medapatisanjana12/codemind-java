2b44868bd8f3719490b3e85940749c0b42a9ee3b
Sanjana Medapati <147472809+Medapatisanjana12@users.noreply.github.com>
2024-07-31 15:27:47 +0530

    import java.util.Scanner; public class Noodle{     public static void main(String[] args){         int x,y,a;         Scanner read=new Scanner(System.in);         x=read.nextInt();         y=read.nextInt();         a=x*y;         System.out.println(a);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Noodle{
+    public static void main(String[] args){
+        int x,y,a;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        y=read.nextInt();
+        a=x*y;
+        System.out.println(a);
+    }
+}
