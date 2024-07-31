aa9e6d279598336f81f9edb409f20152befb7dbe
Sanjana Medapati <147472809+Medapatisanjana12@users.noreply.github.com>
2024-07-31 15:00:07 +0530

    import java.util.Scanner; public class mod{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int a,b,c;         a=read.nextInt();         b=read.nextInt();         c=a%b;         System.out.println(c);     } }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class mod{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int a,b,c;
+        a=read.nextInt();
+        b=read.nextInt();
+        c=a%b;
+        System.out.println(c);
+    }
+}
