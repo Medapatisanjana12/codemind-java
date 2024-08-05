55cf21e3d831833178e4571da5ff2943d95f5033
Sanjana Medapati <147472809+Medapatisanjana12@users.noreply.github.com>
2024-08-05 16:03:17 +0530

    import java.util.Scanner; public class Fuel{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();        if(5*x>=y){            System.out.println("YES");        }        else{            System.out.println("NO");        }     } }

---
+++ Reach_Home.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Fuel{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+       if(5*x>=y){
+           System.out.println("YES");
+       }
+       else{
+           System.out.println("NO");
+       }
+    }
+}
